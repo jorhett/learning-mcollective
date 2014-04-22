4b1e644de284e80f1b2063d6d55b3b51bb7dea71
Jo Rhett <jrhett@netconsonance.com>
2014-04-22 00:35:36 -0700

    Moved to Hiera-defined classes.

--- manifests/site.pm
+++ manifests/site.pm
@@ -1,8 +1,4 @@
+hiera_include('classes')
+
 node default {
-    include mcollective::server
-}
-node 'activemq.example.net' {
-    include mcollective::activemq
-    include mcollective::server
-    include mcollective::client
 }
