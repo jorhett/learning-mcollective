3bff89445c5dee2d9be19127cb8efd9b160b2922
Jo Rhett <jrhett@netconsonance.com>
2014-06-04 03:00:13 -0700

    Added the listener debugger from the book for others to use.

---
+++ examples/listen/debugger.rb
@@ -0,0 +1,55 @@
+# Block of required includes that makes us an MCollective client
+require 'mcollective'
+require 'pp'
+
+Log = MCollective::Log
+
+optparser = MCollective::Optionparser.new
+options = optparser.parse
+
+config = MCollective::Config.instance
+config.loadconfig(options[:config])
+
+MCollective::PluginManager["security_plugin"].initiated_by = :client
+
+connector = MCollective::PluginManager['connector_plugin']
+connector.connect
+
+# Get a reply queue name from anywhere, hardcoded here
+replyqueue = 'debugger'
+queuename = '/queue/mcollective.reply.' + replyqueue
+
+# Here is the queue we want to listen to
+connector.connection.subscribe( queuename, {}, "$$".to_i )
+
+# consume all the things...
+loop do
+  # Get an mcollective Message object and configure it as a reply
+  message = connector.receive
+  message.type = :reply
+  message.decode!          # security plugin validates authentication
+ 
+  # Get the autenticated sender from the envelope
+  sender = message.payload[:senderid]
+  agent = message.payload[:senderagent]
+  body = message.payload[:body]
+ 
+  # Some debugging output (only use one)
+  #pp message
+  #pp message.payload
+  #pp message.payload[:body]
+  
+  # Are we normally shaped reply with a statusmsg?
+  if( body[:statusmsg] )
+    puts sender.to_s + '/' + agent.to_s + ': ' + body[:statusmsg].to_s + '=' + body[:statuscode].to_s
+    if( body[:data].class == Hash )
+      pp body[:data]
+    else
+      pp body
+    end
+  else
+    # things which don't send us a status just pretty-print the body
+    print sender.to_s + ': '
+    pp body
+  end
+end
